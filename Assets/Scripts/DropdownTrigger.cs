c0fca9f6fefc1a4041518b3ae6caae252d65f352
EdoZac <edoardo.zacchi@studenti.unitn.it>
2020-02-01 09:15:43 +0100

    secondo commit

---
+++ Assets/Scripts/DropdownTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownTrigger : MonoBehaviour
+{
+    public bool isActive = false;
+    public Animator animator;
+    public Animation animation;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player") && isActive)
+        {
+            //animator.SetTrigger("DropDown");
+            animation.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerSwitchTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitchTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Cube1"))
+        {
+            GameObject switchPlay = GameObject.FindWithTag("PlayerSwitch");
+            DropdownTrigger dropDown = switchPlay.GetComponent<DropdownTrigger>();
+            dropDown.isActive = true;
+        }
+    }
+}
+
